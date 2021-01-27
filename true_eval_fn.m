164506b37b33739d424226b5d5b721c0f00ec05e
zhongshun <zszhang@outlook.com>
2021-01-27 03:00:07 -0500

    remove unnecessary functions​

--- true_eval_fn.m
+++
@@ -1,3 +0,0 @@
-function val = true_eval_fn(posreward,agent_detected,Teammate_Detection_time_E_smart,Negtive_Reward,Negtive_Teammate,Pr)
-
-val = posreward - Negtive_Reward*agent_detected - Negtive_Teammate*Teammate_Detection_time_E_smart;
